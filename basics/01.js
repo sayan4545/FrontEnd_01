685902884ad308436b48fe3a2efd9aada02a3902
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-05 02:55:41 +0530

    First commit

---
+++ basics/01.js
@@ -0,0 +1,2 @@
+console.log("namaste javascript!!")
+console.log(90+156)
---
+++ basics/01.ts
@@ -0,0 +1,8 @@
+console.log("Hello Js");
+function ts(a, b) {
+    return a + b;
+}
+
+const result = ts(5, 3);
+console.log(result);  // Outputs: 8
+
